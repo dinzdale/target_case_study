b96ff5f9661c98c7c73b20dbbfdc122821664dc2
garyjacobs <gmjacobs61@gmail.com>
2022-11-02 21:38:05 -0400

    Updated deals item 2

--- app/src/main/java/com/target/targetcasestudy/ui/DealItemAdapter.kt
+++ app/src/main/java/com/target/targetcasestudy/ui/DealItemAdapter.kt
@@ -2,6 +2,7 @@ package com.target.targetcasestudy.ui
 
 import android.content.Context
 import android.view.LayoutInflater
+import android.view.RoundedCorner
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ImageView
@@ -9,6 +10,8 @@ import android.widget.TextView
 import androidx.core.content.ContextCompat
 import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
+import com.bumptech.glide.load.resource.bitmap.RoundedCorners
+import com.bumptech.glide.request.RequestOptions
 import com.target.targetcasestudy.R
 import com.target.targetcasestudy.model.Product
 
@@ -26,11 +29,11 @@ class DealItemAdapter(val dealList: List<Product>, val context:Context) : Recycl
 
     override fun onBindViewHolder(viewHolder: DealItemViewHolder, position: Int) {
         viewHolder.salePrice.text = dealList[position].salePrice?.displayString ?: ""
-        viewHolder.regPrice.text = dealList[position].regularPrice?.displayString ?: ""
+        viewHolder.regPrice.text = context.getString(R.string.reg_price,dealList[position].regularPrice?.displayString)
         viewHolder.title.text = dealList[position].title
         viewHolder.fulfillment.text = dealList[position].fulfillment
         dealList[position].imageUrl?.also {
-            Glide.with(context).load(it).placeholder(R.drawable.ic_launcher_foreground).into(viewHolder.productImage)
+            Glide.with(context).load(it).apply(RequestOptions().transform(RoundedCorners(50))).placeholder(R.drawable.ic_launcher_foreground).into(viewHolder.productImage)
         }
     }
 }
