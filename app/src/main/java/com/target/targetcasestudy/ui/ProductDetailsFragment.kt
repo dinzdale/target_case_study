64e7dbe9d39835b530f2e4850fe2675f4a2bb741
garyjacobs <gmjacobs61@gmail.com>
2022-11-03 20:32:41 -0400

    Initial compose integration.

--- app/src/main/java/com/target/targetcasestudy/ui/ProductDetailsFragment.kt
+++ app/src/main/java/com/target/targetcasestudy/ui/ProductDetailsFragment.kt
@@ -5,6 +5,7 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.compose.ui.platform.ComposeView
 import com.target.targetcasestudy.R
 
 // TODO: Rename parameter arguments, choose names that match
@@ -18,22 +19,22 @@ private const val ARG_PARAM2 = "param2"
  * create an instance of this fragment.
  */
 class ProductDetailsFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
+
     }
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_product_details, container, false)
+        //return inflater.inflate(R.layout.fragment_product_details, container, false)
+        return ComposeView(requireContext()).apply {
+            setContent {  }
+        }
+
+
     }
 
     companion object {
