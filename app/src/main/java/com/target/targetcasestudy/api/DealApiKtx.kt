c4f70e21c9bc00eb56aaccbd53b4ed38705f7e67
garyjacobs <gmjacobs61@gmail.com>
2022-11-06 20:47:31 -0500

    Last minute cleanup.

--- app/src/main/java/com/target/targetcasestudy/api/DealApiKtx.kt
+++ app/src/main/java/com/target/targetcasestudy/api/DealApiKtx.kt
@@ -5,12 +5,12 @@ import com.target.targetcasestudy.model.Product
 import retrofit2.http.GET
 import retrofit2.http.Path
 
+internal const val BASE_URL = "https://api.target.com/mobile_case_study_deals/v1/"
+
 interface DealApiKtx {
+    @GET("${BASE_URL}deals")
+    suspend fun retrieveDeals(): ProductResponse
 
-
-  @GET("${BASE_URL}deals")
-  suspend fun retrieveDeals(): ProductResponse
-
-  @GET("${BASE_URL}deals/{dealId}")
-  suspend fun retrieveDeal(@Path("dealId") dealId: Int): Product
+    @GET("${BASE_URL}deals/{dealId}")
+    suspend fun retrieveDeal(@Path("dealId") dealId: Int): Product
 }
--- app/src/main/java/com/target/targetcasestudy/ui/ProductDetailsFragment.kt
+++ app/src/main/java/com/target/targetcasestudy/ui/ProductDetailsFragment.kt
@@ -9,28 +9,21 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.appcompat.app.AppCompatActivity
 import androidx.compose.foundation.*
-import androidx.compose.foundation.gestures.Orientation
-import androidx.compose.foundation.gestures.scrollable
 import androidx.compose.foundation.layout.*
 import androidx.compose.material.*
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.State
-import androidx.compose.runtime.livedata.observeAsState
 import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
-import androidx.compose.ui.modifier.modifierLocalConsumer
 import androidx.compose.ui.platform.ComposeView
 import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.text.font.Font
-import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.text.font.createFontFamilyResolver
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.unit.ExperimentalUnitApi
@@ -38,29 +31,18 @@ import androidx.compose.ui.unit.TextUnit
 import androidx.compose.ui.unit.TextUnitType
 import androidx.compose.ui.unit.dp
 import androidx.constraintlayout.compose.ConstraintLayout
-import androidx.core.graphics.toColor
 import com.target.targetcasestudy.network.DealsViewModel
 import androidx.fragment.app.activityViewModels
 import androidx.navigation.fragment.findNavController
 import com.bumptech.glide.integration.compose.ExperimentalGlideComposeApi
 import com.bumptech.glide.integration.compose.GlideImage
-import com.bumptech.glide.load.resource.bitmap.TransformationUtils
 import com.target.targetcasestudy.R
 import com.target.targetcasestudy.model.ItemNotFoundResponse
 import com.target.targetcasestudy.model.Product
 import com.target.targetcasestudy.model.getErrorResponse
 import kotlinx.coroutines.launch
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
 
-/**
- * A simple [Fragment] subclass.
- * Use the [ProductDetailsFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
 class ProductDetailsFragment : Fragment() {
 
     val dealsViewModel: DealsViewModel by activityViewModels()
@@ -72,11 +54,9 @@ class ProductDetailsFragment : Fragment() {
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?): View? {
-        // Inflate the layout for this fragment
-        //return inflater.inflate(R.layout.fragment_product_details, container, false)
+
         return ComposeView(requireContext()).apply {
             setContent {
-
                 MaterialTheme {
                     Surface() {
                         BuildUI()
@@ -84,8 +64,6 @@ class ProductDetailsFragment : Fragment() {
                 }
             }
         }
-
-
     }
 
 
@@ -262,23 +240,4 @@ class ProductDetailsFragment : Fragment() {
         super.onAttach(context)
     }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment ProductDetailsFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            ProductDetailsFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
-    }
 }
